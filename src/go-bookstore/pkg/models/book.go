7b13e0d5e4f8a475aaa60f7227cb1eafbb5e5599
dgarcia <manuonda@gmail.com>
2022-08-04 19:17:58 -0300

    routes projects

--- src/go-bookstore/pkg/models/book.go
+++ src/go-bookstore/pkg/models/book.go
@@ -1,1 +1,43 @@
 package models
+
+import (
+	"../config"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+type Book struct {
+	Name        string `json:"name"`
+	Author      string `json:"author"`
+	Publication string `json:"publication"`
+}
+
+func init() {
+	config.Connect()
+	db = config.getDb()
+	db.AutoMigrate(&Book{})
+}
+
+func (b *Book) CreateBook() *Book {
+	db.Create(&b)
+	return b
+}
+
+func GetAllBooks() []Book {
+	var Books []Book
+	db.Find(&Books)
+	return Books
+}
+
+func GetBookById(Id int64) (*Book, *gorm.DB) {
+	var book Book
+	db := db.Where("ID=?", Id).Find(&book)
+	return &book, db
+}
+
+func DeleteBook(Id int64) Book {
+	var book Book
+	db.Where("ID=?", Id).Delete(&book)
+	return book
+}
