91999ff9f8cd55367d511cd3910d2fe76d697bb4
manuonda <manuonda@gmail.com>
2022-08-15 17:46:34 -0300

    modificate go

--- src/cli-todo/main.go
+++ src/cli-todo/main.go
@@ -1,7 +1,10 @@
 package mian
 
 import (
+	"encoding/json"
 	"errors"
+	"io/ioutil"
+	"os"
 	"time"
 )
 
@@ -42,5 +45,29 @@ func (t *Todos) Delete(index int) error {
 
 	*t = append(ls[:index-1], ls[index:]...)
 	return nil
+
+}
+
+func (t *Todos) Load(filename string) error {
+	file, err := ioutil.ReadFile(filename)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return nil
+		}
+		return err
+	}
+	if len(file) == 0 {
+		return err
+	}
+
+	err = json.Unmarshal(file, t)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (t *Todos) Store(filename string) error {
+	data, err := json.Marshal(t)
 
 }
