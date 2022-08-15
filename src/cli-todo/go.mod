d365c4d33449d0d0b0a4e3bedfa3cd4b958b9da0
manuonda <manuonda@gmail.com>
2022-08-15 17:31:37 -0300

    create project cli go

---
+++ src/cli-todo/go.mod
@@ -0,0 +1,3 @@
+module github.com/manuonda/go-projects/src/cli-todo
+
+go 1.18
--- src/scrapper-web2/main.go
+++ src/scrapper-web2/main.go
@@ -8,17 +8,17 @@ import (
 )
 
 func main() {
-	var url = "https://saphirus.com.ar/vende-saphirus/"
+	var url = "https://tienda.iberiahogar.com.ar/limpieza/?mpage=2&gclid=Cj0KCQjwrs2XBhDjARIsAHVymmSFVwaW0ybAILv2oTJI5shBFpuMm1vjoeIC-9AER4WzX64aF-4Uh5MaAi8QEALw_wcB"
 	response, error := http.Get(url)
 	if error != nil {
 		log.Fatal(error)
 	}
 
-	fmt.Print(response)
+	// fmt.Print(response)
 	status, err := strconv.ParseInt(response.Status, 6, 12)
-
+	fmt.Printf("Status %d ", status)
 	if err != nil {
-		fmt.Print("E")
+		fmt.Print("Error response status")
 	}
 	if status > 400 {
 		fmt.Printf("Status Code : %s")
