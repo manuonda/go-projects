3ee29b9f4d3884230c07b510e96e1eb577e78cc5
David Garcia <david.garcia@grupomost.com>
2023-01-26 20:22:04 -0300

    modificate files

--- cli-tool-simple/cobra-cli/main.go
+++ cli-tool-simple/cobra-cli/main.go
@@ -15,7 +15,7 @@ limitations under the License.
 */
 package main
 
-import "cli-tool-simple/cobra-cli/cmd"
+import "github.com/manuonda/go-projects/cli-tool-simple/cobra-cli/cmd"
 
 func main() {
 	cmd.Execute()
