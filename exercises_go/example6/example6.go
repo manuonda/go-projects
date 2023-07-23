e36f6ce41c1a43e231492ce1e4e42aefde33e7da
dgarcia <manuonda@gmail.com>
2023-07-23 00:08:21 -0300

    add file transform

--- exercises_go/example6/example6.go
+++ exercises_go/example6/example6.go
@@ -4,8 +4,6 @@ import (
 	"flag"
 	"fmt"
 	"os"
-
-	"gopkg.in/yaml.v3"
 )
 
 func main() {
@@ -29,23 +27,4 @@ func main() {
 		fmt.Println(" %s (%s)", instructor.Name, instructor.Email)
 	}
 
-}
-func tarnsform(fileName string) (WorkShop, error) {
-
-	var jsonData WorkShop
-
-	file, error := os.ReadFile(fileName)
-	if error != nil {
-		fmt.Println("Could not Read File", flag.Args())
-		os.Exit(1)
-	}
-
-	error = yaml.Unmarshal(file, &jsonData)
-
-	if error != nil {
-		fmt.Println("Could Not Unmarshall Decode", error.Error())
-		os.Exit(1)
-	}
-
-	return jsonData, nil
 }
